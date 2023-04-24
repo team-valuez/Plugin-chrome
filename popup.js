b518c067273a64d8909e938d6cfeb5f287d88af7
flicker <flickerflak@gmail.com>
2023-04-24 12:07:25 +0530

    add features

--- popup.js
+++ popup.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('form');
-  const inputField = document.getElementById('input-field');
+  const urlField = document.getElementById('url');
+  const textField = document.getElementById('myTextarea');
   const responseElement = document.getElementById('response');
 
   form.addEventListener('submit', function(event) {
     event.preventDefault(); // prevent the form from submitting normally
-    var text = inputField.value;
-    const data = { id: 0,
-                  name :text};
+    var url = urlField.value;
+    var txt = textField.value;
+    const data = {
+                  url :url,
+                  text:txt  
+                };
     fetch('http://127.0.0.1:5000/names', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
